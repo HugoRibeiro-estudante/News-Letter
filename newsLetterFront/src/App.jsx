22177cb8168f2052c0de56969191a0f359e28f76
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2023-10-22 12:14:05 -0300

    front end added

---
+++ newsLetterFront/src/App.jsx
@@ -0,0 +1,13 @@
+import Notification from './components/Notification';
+
+function App() {
+  return (
+    <div>
+      <header> 
+      <Notification />
+    </header> 
+  </div>
+  );
+}
+
+export default App;
